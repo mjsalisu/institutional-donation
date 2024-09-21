6b271d1e7d5508d5a70d868ceb9d008190a3be2c
Jamilu Salisu <jamilusalis@gmail.com>
2024-09-21 15:20:12 +0100

    new db file

--- app/DB/idbs_db.sql
+++ app/DB/idbs_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 17, 2024 at 08:02 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Host: localhost:8889
+-- Generation Time: Sep 21, 2024 at 02:06 PM
+-- Server version: 5.7.39
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,8 +33,15 @@ CREATE TABLE `donations` (
   `donatedTo` varchar(20) NOT NULL,
   `amount` varchar(100) NOT NULL,
   `remark` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp()
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `donations`
+--
+
+INSERT INTO `donations` (`id`, `donatedBy`, `donatedTo`, `amount`, `remark`, `createdAt`) VALUES
+(20, '5', '69', '60000', '', '2024-05-02 12:45:15');
 
 -- --------------------------------------------------------
 
@@ -50,15 +57,43 @@ CREATE TABLE `donors` (
   `password` varchar(20) NOT NULL,
   `occupation` varchar(100) NOT NULL,
   `about` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp()
+  `role` int(10) NOT NULL DEFAULT '1' COMMENT '0=admin, 1=donor',
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `donors`
 --
 
-INSERT INTO `donors` (`id`, `name`, `phone`, `email`, `password`, `occupation`, `about`, `createdAt`) VALUES
-(1, 'Yunus Isah', '09033248408', 'yunusisah123@gmail.com', '12345', '', NULL, '2024-02-11 14:20:39');
+INSERT INTO `donors` (`id`, `name`, `phone`, `email`, `password`, `occupation`, `about`, `role`, `createdAt`) VALUES
+(4, 'Sys Admin', '08011112220', 'admin@email.com', 'admin', 'Department Administrator', 'SWE Sys Admin', 0, '2024-02-20 04:07:12'),
+(5, 'Kano State Gov', '09022334455', 'kano@email.com', 'kano', 'Politics', NULL, 1, '2024-05-31 07:07:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `id` int(20) NOT NULL,
+  `requestID` varchar(100) NOT NULL,
+  `studentID` varchar(20) NOT NULL,
+  `studentNote` varchar(500) NOT NULL,
+  `remarkNote` varchar(500) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`id`, `requestID`, `studentID`, `studentNote`, `remarkNote`, `created_at`, `updated_at`) VALUES
+(9, 'R5X79C', '69', 'I seek for help to cover my fees', 'Best of luck! Your application has been approved.', '2024-05-02 12:06:12', '2024-05-02 11:15:55'),
+(10, 'HHWBTZ', '68', 'I need help to cover my school fees, please.', NULL, '2024-05-31 06:03:42', NULL),
+(11, 'UXRMZB', '67', 'NA', 'Best of luck in your endeavour', '2024-05-31 06:04:32', '2024-05-31 06:05:18'),
+(12, 'THPD0J', '66', 'I have no means to pay my school fees. Please help me so others will support my education. Thank you.', 'Your CGPA didn’t meet the minimum requirement. Apologies and we hope you could secure funds using other medium.', '2024-05-31 06:05:58', '2024-05-31 05:07:49');
 
 -- --------------------------------------------------------
 
@@ -77,9 +112,9 @@ CREATE TABLE `students` (
   `regno` varchar(50) NOT NULL,
   `level` varchar(10) NOT NULL,
   `cgpa` varchar(5) NOT NULL,
-  `disability` varchar(20) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `status` int(11) NOT NULL
+  `disability` varchar(100) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -87,15 +122,19 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `phone`, `email`, `gender`, `state`, `lga`, `regno`, `level`, `cgpa`, `disability`, `createdAt`, `status`) VALUES
-(19, 'Yunus Isah', '09033248408', 'yunusisah123@gmail.com', 'Male', 'Kano', 'Rogo', 'CST/17/IFT/00029', '400', '5.00', 'Good', '2024-02-15 12:40:41', 0);
+(66, 'Barira Saleh', '09011223344', 'test@abc029', 'female', 'Kano', 'Ungogo', 'CST/19/SWE/00529', '200', '2.2', 'Physical Disability', '2024-05-02 11:55:10', 'Rejected'),
+(67, 'Beaulah Reinger', '08022334455', 'test@abc007', 'female', 'Kano', 'Bebeji', 'CST/19/SWE/00507', '400', '3.8', 'Healthy', '2024-05-02 11:55:10', 'Approved'),
+(68, 'Craig Douglas', '07033445566', 'test@abc018', 'female', 'Kaduna', 'Zaria', 'CST/19/SWE/00518', '300', '4.5', 'Healthy', '2024-05-02 11:55:10', 'Pending'),
+(69, 'Jamilu Salisu', '08028752833', 'jamilusalis@gmail.com', 'male', 'Kano', 'Bebeji', 'CST/19/SWE/00409', '400', '3.8', 'Hearing Impairment', '2024-05-02 11:58:12', 'Approved'),
+(71, 'Salisu Jamila', '08011223344', 'jamilasalis@gmail.com', 'female', 'Adamawa', 'Song', 'CST/20/SWE/500', '200', '4.8', 'Healthy', '2024-05-31 07:01:40', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_configuration`
+-- Table structure for table `system_config`
 --
 
-CREATE TABLE `system_configuration` (
+CREATE TABLE `system_config` (
   `id` int(11) NOT NULL,
   `faculty` varchar(200) NOT NULL,
   `department` varchar(250) NOT NULL,
@@ -106,39 +145,11 @@ CREATE TABLE `system_configuration` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `system_configuration`
+-- Dumping data for table `system_config`
 --
 
-INSERT INTO `system_configuration` (`id`, `faculty`, `department`, `fees_amount`, `application_deadline`, `bank_name`, `account_number`) VALUES
-(1, 'Faculty Of Computing', 'Software Engineering', '100,000', '2024-03-08', 'GTB', '4055345446');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `regNo` varchar(30) DEFAULT NULL,
-  `faculty` varchar(50) DEFAULT NULL,
-  `department` varchar(50) DEFAULT NULL,
-  `phone` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `role` int(2) NOT NULL COMMENT '0 = user, 1 = admin',
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `regNo`, `faculty`, `department`, `phone`, `password`, `role`, `created_at`) VALUES
-(4, 'Administrator', 'admin@email.com', NULL, NULL, NULL, '08011223344', 'admin', 1, '2023-11-17 23:32:20'),
-(5, 'User', 'user@email.com', NULL, NULL, NULL, '07066778899', 'user', 0, '2023-11-17 23:41:30'),
-(7, 'Jamilu Salisu', 'jamilusalis@gmail.com', NULL, NULL, NULL, '08028752833', 'mjtech', 0, '2023-11-19 12:51:48');
+INSERT INTO `system_config` (`id`, `faculty`, `department`, `fees_amount`, `application_deadline`, `bank_name`, `account_number`) VALUES
+(1, 'Faculty of Computing', 'Software Engineering', '100,000', '2024-05-31', 'Zenith Bank', '0005556677');
 
 -- --------------------------------------------------------
 
@@ -151,8 +162,16 @@ CREATE TABLE `wallet` (
   `donatedBy` int(20) NOT NULL,
   `amount` varchar(100) NOT NULL,
   `remark` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp()
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Department Wallet';
+
+--
+-- Dumping data for table `wallet`
+--
+
+INSERT INTO `wallet` (`id`, `donatedBy`, `amount`, `remark`, `createdAt`) VALUES
+(10, 4, '30000', '', '2024-05-02 12:35:49'),
+(11, 4, '50000', '', '2024-05-31 07:02:18');
 
 --
 -- Indexes for dumped tables
@@ -171,21 +190,21 @@ ALTER TABLE `donors`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `system_configuration`
+-- Indexes for table `system_config`
 --
-ALTER TABLE `system_configuration`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
+ALTER TABLE `system_config`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -202,37 +221,37 @@ ALTER TABLE `wallet`
 -- AUTO_INCREMENT for table `donations`
 --
 ALTER TABLE `donations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `donors`
 --
 ALTER TABLE `donors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
--- AUTO_INCREMENT for table `system_configuration`
+-- AUTO_INCREMENT for table `system_config`
 --
-ALTER TABLE `system_configuration`
+ALTER TABLE `system_config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `wallet`
 --
 ALTER TABLE `wallet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
